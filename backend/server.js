943774dc90ff9dd2b3b510b5a37e525fcb14bec1
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-11 22:39:19 +0100

    Update CORS origin to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");   // ← VETËM NJË HERË
+const cors = require("cors");   
 const path = require("path");
 const connectDB = require("./config/db");
 
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "https://expensetracker-tnaw.onrender.com",
+  origin: process.env.CLIENT_URL, 
   credentials: true
 }));
 
