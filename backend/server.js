dea06e91e5fb501b815471967e5266c198fb749f
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-12 00:46:14 +0100

    Update CORS configuration to use a specific origin

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL, 
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://expensetracker-tnaw.onrender.com",
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
