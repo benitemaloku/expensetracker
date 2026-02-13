1757162db078f51be9dad2c54ef4c186df403354
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-14 00:15:16 +0100

    Update axios instance base URL

--- frontend/expense-tracker/src/utils/axiosinstance.js
+++ frontend/expense-tracker/src/utils/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL, 
+  baseURL: "https://expensetracker-1-o780.onrender.com/api/v1", 
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
