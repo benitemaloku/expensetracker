aa8269d8e9fe0af46c1d5ae0a89e85a7fdefa23f
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-13 22:36:22 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,12 @@ const app = express();
 
 connectDB();
 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+
 app.use(
   cors({
     origin: "https://expensetracker-tnaw.onrender.com",
