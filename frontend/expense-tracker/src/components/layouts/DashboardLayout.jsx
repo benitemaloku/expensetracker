f341e2789419c17ba22a51f0416a33a0c8de3ae3
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-14 01:19:29 +0100

    Refactor DashboardLayout to include EditProfile component

--- frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
+++ frontend/expense-tracker/src/components/layouts/DashboardLayout.jsx
@@ -1,31 +1,43 @@
-import React, { useContext } from 'react';
-import UserProvider, { UserContext } from '../../context/UserProvider';
-import Navbar from './Navbar';
-import SideMenu from './SideMenu';
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../context/UserProvider";
+import Navbar from "./Navbar";
+import SideMenu from "./SideMenu";
+import EditProfile from "./EditProfile";
 
 const DashboardLayout = ({ children, activeMenu }) => {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  const [openEditProfile, setOpenEditProfile] = useState(false);
 
-    if (user === undefined) {
-        return <p>Loading...</p>;
-    }
+  if (user === undefined) {
+    return <p>Loading...</p>;
+  }
 
-    return (
-        <div>
-            <Navbar activeMenu={activeMenu} />
+  return (
+    <div>
+      <Navbar
+        activeMenu={activeMenu}
+        onOpenEditProfile={() => setOpenEditProfile(true)}
+      />
 
-            {user ? (
-                <div className='flex'>
-                    <div className='max-[1080px]:hidden'>
-                        <SideMenu activeMenu={activeMenu} />
-                    </div>
-                    <div className='grow mx-5'>{children}</div>
-                </div>
-            ) : (
-                <p className='text-center mt-10'></p>
-            )}
+      {user ? (
+        <div className="flex">
+          <div className="max-[1080px]:hidden">
+            <SideMenu
+              activeMenu={activeMenu}
+              openEditProfile={() => setOpenEditProfile(true)}
+            />
+          </div>
+
+          <div className="grow mx-5">{children}</div>
         </div>
-    );
+      ) : null}
+
+      <EditProfile
+        isOpen={openEditProfile}
+        onClose={() => setOpenEditProfile(false)}
+      />
+    </div>
+  );
 };
 
 export default DashboardLayout;
