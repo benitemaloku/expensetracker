b1bd11e008ba0a7c724251dfa82595ca0c88e5b2
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-15 16:53:54 +0000

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { registerUser, loginUser, getUserInfo, updateUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  getUserInfo,
+  updateUser,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/authController");
 const upload = require("../middleware/uploadMiddleware");
 
 const router = express.Router();
 
+// Auth routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
 router.put("/updateUser", protect, upload.single("profileImage"), updateUser);
 
+// Password reset routes
+router.post("/forgotPassword", forgotPassword);
+router.put("/reset/:token", resetPassword);
 
+// Image upload route
 router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
