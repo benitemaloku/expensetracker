12c26f757e9c8ecf7ebe83ece43cba75e0f66bf7
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-16 18:35:57 +0000

    Update SideMenu.jsx

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -30,7 +30,7 @@ const SideMenu = ({ activeMenu, openEditProfile = () => {} }) => {
   };
 
   return (
-    <div className="w-64 h-full bg-white border-r border-gray-200 p-5 flex flex-col">
+   <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-grey-200/50 p-5 sticky top-[61px] z-20 flex flex-col">
       {/* User info */}
       <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
         {user?.profileImageUrl ? (
