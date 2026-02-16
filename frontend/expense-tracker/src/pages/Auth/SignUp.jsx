922922ee48d3930a659214b4b97e077e94d95c84
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-16 19:09:41 +0000

    Update SignUp.jsx

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -66,13 +66,13 @@ const SignUp = () => {
 
   return (
     <AuthLayout>
-      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
         <h3 className="text-xl font-semibold text-black">Create an Account</h3>
         <p className="text-xs text-slate-700 mt-[5px] mb-6">
           Join us today by entering your details below.
         </p>
 
-        <form onSubmit={handleSignUp} className="flex flex-col gap-1.5">
+        <form onSubmit={handleSignUp} className="flex flex-col">
           <Input
             value={fullName}
             onChange={({ target }) => setFullName(target.value)}
