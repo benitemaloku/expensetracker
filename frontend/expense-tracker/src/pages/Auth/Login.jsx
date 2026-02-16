89fda9714118b0c071edf3f8d568150463e05575
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-16 19:09:13 +0000

    Update Login.jsx

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -60,7 +60,7 @@ export const Login = () => {
       <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
         <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
         <p className='text-xs text-slate-700 mt-[5px] mb-6'>
-          Plaease enter your details to log in
+          Plaease enter your details to log in to your account.
         </p>
 
  <form onSubmit={handleLogin}>
