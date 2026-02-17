4209b3ca368df88be4af3c3d7573e95735308ee4
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-17 19:35:11 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,29 +12,23 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-// Në rast që jeni prapa proxy (Render, Heroku, etj)
 app.set("trust proxy", 1);
 
-// Lidhja me MongoDB
 connectDB();
 
-// Middleware për JSON
 app.use(express.json());
 
-// CORS për frontend
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // URL e frontend-it
+    origin: process.env.CLIENT_URL, 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // nevojitet për cookies
+    credentials: true, 
   })
 );
 
-// Folder për static files (p.sh. uploaded images)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Test route bazë
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -48,7 +42,6 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 // Port
 const PORT = process.env.PORT || 8000;
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
