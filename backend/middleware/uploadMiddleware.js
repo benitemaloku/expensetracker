402fa1c65addc615f07bcd2da91b536e17f0b914
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-17 17:50:20 +0100

    Update uploadMiddleware.js

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,22 +1,32 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
+// Absolute path për uploads folder
+const uploadPath = path.join(__dirname, "../uploads");
+
+// Krijo folderin nëse nuk ekziston
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads"); 
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`); 
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-//File filter
+// File filter
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only .jpeg, .jpg dhe .png formats are allowed"), false);
+    cb(new Error("Only .jpeg, .jpg and .png formats are allowed"), false);
   }
 };
 
