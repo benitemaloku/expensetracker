346507fb56023886e2cef120677ead8a274f001d
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-17 19:32:51 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,8 +21,6 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-
-
 UserSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
