8c8fe635f1d3488b9ba7e32b82c768186595176e
Benitë Maloku <157824369+benitemaloku@users.noreply.github.com>
2026-02-17 18:54:29 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -160,8 +160,8 @@ exports.forgotPassword = async (req, res) => {
     const resetUrl = `${process.env.CLIENT_URL}/reset-password/${resetToken}`;
 
     const message = `
-      <h2>Hi, <br> Sorry to hear you’re having trouble logging into Instagram. We got a message that you forgot your password. If this was you, you can reset your password now.</h2>
-      <p>You requested a password reset.</p>
+      <p>Hi,</p>
+      <p>Sorry to hear you’re having trouble logging into Instagram. We got a message that you forgot your password. If this was you, you can reset your password now.</p>
       <a href="${resetUrl}" 
          style="display:inline-block;padding:10px 20px;background:#4F46E5;color:#fff;text-decoration:none;border-radius:5px;">
          Reset your password
